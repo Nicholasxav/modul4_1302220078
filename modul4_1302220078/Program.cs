af96c61dee69e190118a3848f9b33ecd9f48d49b
PRAKTIKAN <PRAKTIKAN@DESKTOP-2G1A4E6>
2024-03-13 07:06:49 +0700

    first commit

---
+++ modul4_1302220078/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+public class KodeBUah
+{
+    public string GetKodeBuah (String namaBuah)
+    {
+        if (namaBuah == "apel")
+        {
+            return "A00";
+        }
+        else if (namaBuah == "Aprikot")
+        {
+            return "B00";
+        }
+        else if (namaBuah == "Alpukat")
+        {
+            return "C00";
+        }
+        else if (namaBuah == "Pisang")
+        {
+            return D00;
+        } else if (namaBuah == "Paprika")
+        {
+            return E00;
+        }
+    }
+}
+s
